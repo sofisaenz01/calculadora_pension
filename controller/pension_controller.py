3cf9cdf8f0f95ef1081ffff5122ba177657e3349
sofisaenz01 <sofisaenz992@gmail.com>
2025-03-31 20:33:19 -0500

    html 1

--- controller/pension_controller.py
+++ controller/pension_controller.py
@@ -1,1 +1,7 @@
-print("sofi")
+#
+#from django.urls import path
+#from views import run, carrito, factura, metodo, pago
+
+#urlpatterns = [
+#    path('', run, name='index'),  # Vista para la página principal de ventas
+# ]
